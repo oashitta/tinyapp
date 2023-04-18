3421bf61f576c0075592ac4c3e7681ccdccc6cbd
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-18 16:20:35 -0400

    updated server so that newly generated keys are saved to the database

--- express_server.js
+++ express_server.js
@@ -10,6 +10,16 @@ const urlDatabase = {
   "9sm5xK": 'http://www.google.com'
 };
 
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++){
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 app.get('/', (reg, res) => {
   res.send('Hello!');
 });
@@ -24,23 +34,34 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  const templateVars = { id: req.params.id, longURL: longURL};
+  const longURL = urlDatabase[req.params.id].longURL;
+  // console.log(longURL)
+  const templateVars = { id: req.params.id, longURL};
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log(req.body)
+  const id = generateRandomString(6)
+  // console.log(id)
+  urlDatabase[id] = req.body
+  console.log(urlDatabase)
+  res.redirect(`/urls/${id}`)
+
+  // res.render("urls_new");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.redirect("/urls")
 });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 });
 
-function generateRandomString() {}
